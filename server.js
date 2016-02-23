182bfc3f87cf6cbed4923b1cd4c708de6dffdfa7
Michael VanderHelm <mikevh@gmail.com>
2016-02-22 16:39:04 -0800

    angular

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-   res.send('hello world'); 
-});
+app.use(express.static('public'))
 
 app.listen(3000, function() {
    console.log('app running on http://localhost:3000'); 
